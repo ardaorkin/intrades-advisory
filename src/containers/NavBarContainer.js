14863332f8d4cb338e06f07de8cd74877b247c95
Arda Örkin <ardaorkin@gmail.com>
2022-08-31 22:22:27 +0300

    responsive navbar

--- src/containers/NavBarContainer.js
+++ src/containers/NavBarContainer.js
@@ -5,15 +5,24 @@ import Navbar from "react-bootstrap/Navbar";
 function NavBarContainer() {
   return (
     <>
-      <Navbar sticky="top" variant="dark" className="navbar-container">
+      <Navbar
+        collapseOnSelect
+        expand="lg"
+        sticky="top"
+        variant="dark"
+        className="navbar-container"
+      >
         <Container>
           <Navbar.Brand href="#home">Intrades Advisory</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#about">About Us</Nav.Link>
-            <Nav.Link href="#industries">Industries</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-            <Nav.Link href="#contact">Contact Us</Nav.Link>
-          </Nav>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#about">About Us</Nav.Link>
+              <Nav.Link href="#industries">Industries</Nav.Link>
+              <Nav.Link href="#pricing">Pricing</Nav.Link>
+              <Nav.Link href="#contact">Contact Us</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
     </>
