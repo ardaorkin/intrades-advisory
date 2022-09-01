73cb30b32bc9a7c910d16c69a72bfa0ff0d47859
Arda Örkin <ardaorkin@gmail.com>
2022-09-01 07:49:33 +0300

    glass

--- src/components/IndustryCard.js
+++ src/components/IndustryCard.js
@@ -2,7 +2,16 @@ import Card from "react-bootstrap/Card";
 
 function IndustryCard(props = { text: "", title: "", image: "" }) {
   return (
-    <Card style={{ width: "18rem", color: "black", minHeight: "10rem" }}>
+    <Card
+      style={{
+        width: "18rem",
+        minHeight: "10rem",
+        backgroundColor: "transparent",
+        border: "1px solid rgba(22, 225, 208, 0.33)",
+        boxShadow: "0 4px 30px rgba(0, 0, 0, 0.1)",
+        backdropFilter: "blur(5px)",
+      }}
+    >
       <Card.Img variant="top" src={props.image} />
       <Card.Body>
         <Card.Title>{props.title}</Card.Title>
